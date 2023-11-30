41a093790fe5f57f8baa9a3d10e7c31d02be5564
craftsmanshared <shared@craftsmansolutions.com>
2023-11-30 14:37:50 -0800

    fixing return_url_prefix to just root

--- src/Libraries/Nop.Services/Media/RoxyFileman/RoxyFilemanFileProvider.cs
+++ src/Libraries/Nop.Services/Media/RoxyFileman/RoxyFilemanFileProvider.cs
@@ -319,7 +319,7 @@ namespace Nop.Services.Media.RoxyFileman
 
                 //no need user to configure
                 INTEGRATION = "custom",
-                RETURN_URL_PREFIX = $"{pathBase}/images/uploaded/",
+                RETURN_URL_PREFIX = $"{pathBase}/",
                 DIRLIST = $"{pathBase}/Admin/RoxyFileman/DirectoriesList",
                 CREATEDIR = $"{pathBase}/Admin/RoxyFileman/CreateDirectory",
                 DELETEDIR = $"{pathBase}/Admin/RoxyFileman/DeleteDirectory",
