551e6846a1f459f4a7af6bb0a796c98b2d854d7e
Sam Elmore <sam@craftsmansolutions.com>
2024-12-03 18:53:53 -0700

    Albina NopCommerce: * adding bulk email click through redirector

---
+++ src/Libraries/Nop.Services/BulkEmail/BulkEmailService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using LinqToDB;
+using LinqToDB.Data;
+using Nop.Data;
+
+namespace Nop.Services.BulkEmail
+{
+    public class BulkEmailService : IBulkEmailService
+    {
+        private readonly INopDataProvider _dataProvider;
+
+        public BulkEmailService(INopDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+        }
+
+        public async Task<string> RegisterClickAndGetRedirectUrlAsync(Guid redirectGuid)
+        {
+            var guidParam = new DataParameter("bulkEmailContactLinkID", redirectGuid, DataType.Guid);
+            var results = await _dataProvider.QueryProcAsync<string>("BulkEmailContactLink_RegisterClick", guidParam);
+            var result = results.Count > 0 ? results.First() : null;
+            return result != null && !string.IsNullOrWhiteSpace(result) ? result : null;
+            
+        }
+    }
+}
